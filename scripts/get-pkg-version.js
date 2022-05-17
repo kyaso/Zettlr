4bf1e6dff470fea60930675369bb229a6144f6a0
Kirthihan Yasotharan <kitti2295+git@gmail.com>
2022-05-17 21:49:03 +0200

    chore: Change way how version is retrieved

--- scripts/get-pkg-version.js
+++ scripts/get-pkg-version.js
@@ -4,8 +4,4 @@ const path = require('path')
 
 const ver = require(path.join(__dirname, '../package.json')).version
 
-// We need the version from package.json solely for identifying the builds.
-// Electron builder, however, which is building them, simply decides to snip
-// the "+"-part for our nightlies. So we have to do the same so the workflow
-// doesn't fail and, by the way, electron-builder: WHY THE F***?
-console.log(ver.indexOf('+') > -1 ? ver.split('+')[0] : ver)
+console.log(ver)
