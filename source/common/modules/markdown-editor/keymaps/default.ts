b506975cf8c1b7acd363a122cc370c7c6c8bf5f5
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2025-08-26 22:01:48 +0200

    fix(custom): Disable conflicting key maps

--- source/common/modules/markdown-editor/keymaps/default.ts
+++ source/common/modules/markdown-editor/keymaps/default.ts
@@ -235,8 +235,8 @@ export function defaultKeymap (): Extension {
     // Table Editor Keys. These need to be the last, since they override some
     // commands and need to only run if nothing equivalently mapped can be run
     // within the corresponding cells.
-    { key: 'Ctrl-l', run: alignLeft, preventDefault: true },
-    { key: 'Ctrl-c', run: alignCenter, preventDefault: true },
+    // { key: 'Ctrl-l', run: alignLeft, preventDefault: true },
+    // { key: 'Ctrl-c', run: alignCenter, preventDefault: true },
     { key: 'Ctrl-r', run: alignRight, preventDefault: true },
     { key: 'Mod-Shift-a', run: v => alignTables(v, v.state.selection.main.head) },
     { key: 'Alt-ArrowUp', run: swapPrevRow, shift: addRowBefore },
