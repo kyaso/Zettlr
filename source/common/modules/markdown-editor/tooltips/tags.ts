93604b8407a982abc253158c6b92ca82d978268a
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2026-01-29 21:18:58 +0100

    fix(custom): Use new types for tag tooltip

--- source/common/modules/markdown-editor/tooltips/tags.ts
+++ source/common/modules/markdown-editor/tooltips/tags.ts
@@ -20,7 +20,7 @@ import { getSearchButton, getCopyButton } from './common'
 async function tagTooltip (view: EditorView, pos: number): Promise<Tooltip|null> {
   const nodeAt = syntaxTree(view.state).resolve(pos, 0)
 
-  if (nodeAt.type.name !== 'ZknTagContent') {
+  if (![ 'ZknTag', 'ZknTagMark' ].includes(nodeAt.type.name)) {
     return null
   }
 
